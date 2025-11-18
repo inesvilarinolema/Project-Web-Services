3b99f951336a7c846de9e7781c05c8d3631014c6
Mariusz Jarocki <mariusz.jarocki@wmii.uni.lodz.pl>
2025-11-18 06:47:27 +0100

    Authorization frontend

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,13 @@
-import { Routes } from '@angular/router';
+import { Route } from '@angular/router';
 import { HomePage } from './pages/home/home';
 import { PersonsPage } from './pages/persons/persons';
 
-export const routes: Routes = [
-  { path: '', component: HomePage },
-  { path: 'persons', component: PersonsPage },
+export interface AppRoute extends Route {
+  icon?: string;
+  roles?: number[];
+}
+
+export const routes: AppRoute[] = [
+  { path: '', component: HomePage, title: 'Home', icon: 'home' },
+  { path: 'persons', component: PersonsPage, title: 'Persons', icon: 'people' }
 ];
